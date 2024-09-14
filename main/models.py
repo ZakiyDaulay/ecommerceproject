3d95ea102d08d6985d2bc94a80621d88c69064c2
ZakiyDaulay <zakiydaulay@gmail.com>
2024-09-14 15:37:08 +0700

    fixed the id in models.py

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 
 class ProductEntry(models.Model):
+    id = models.AutoField(primary_key=True) 
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
