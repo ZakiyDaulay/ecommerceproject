e80294251441f72e70e3c8ae086dd39eae9b947f
ZakiyDaulay <zakiydaulay@gmail.com>
2024-09-25 10:14:18 +0700

    fixed heading in product entry

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class ProductEntry(models.Model):
-    id = models.AutoField(primary_key=True) 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
